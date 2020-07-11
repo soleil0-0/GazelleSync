ae03f4efb30939e3ead840cfcd6122ac3574a7b6
soleil <soleil0_0@outlook.com>
2020-07-11 21:03:27 +0800

    Update console msg when log in failed using username and password

--- redapi.py
+++ redapi.py
@@ -89,7 +89,7 @@ class RedApi:
         }
 		r = self.session.post(loginpage, data=data)
 		if r.status_code != 200 or not r.url.endswith('index.php'):
-			raise LoginException("Login <%s> failed, username or password may be incorrect" % self.site)
+			raise LoginException("Login <%s> failed, username and password may be incorrect or login too frequency" % self.site)
 		self._auth()
 
 	def logout(self):
